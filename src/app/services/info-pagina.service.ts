f7267a338cf604657c0a31930135496668070018
seguracarlos <segurac.carlos@gmail.com>
2018-12-04 17:52:12 -0600

    cambios

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfoPaginaService {
 
-  constructor() {
+  constructor(private http: HttpClient ) {
     console.log('Servicio de infoPagina listo');
+
+    // Leer el archivo Json
+    this.http.get('assets/data/data-pagina.json')
+    .subscribe (resp => {
+
+      console.log(resp);
+    });
    }
 }
